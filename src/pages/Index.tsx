d2b077c32d8b6fe924650429df32fe66486855f8
Shresth Pandey <shresthpandey2004@gmail.com>
2026-01-23 22:31:43 +0530

    Revert "Simplify homepage - remove all extra sections, keep only Hero and BestSellers"
    This reverts commit 6daa01c498904771ead82eba1d71dd5215f9ce44.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,13 @@ import Footer from '@/components/layout/Footer';
 import CartDrawer from '@/components/cart/CartDrawer';
 import WhatsAppChatWidget from '@/components/common/WhatsAppChatWidget';
 import HeroSection from '@/components/home/HeroSection';
+import NewLaunchSection from '@/components/home/NewLaunchSection';
 import BestSellersSection from '@/components/home/BestSellersSection';
+import WhyChooseUsSection from '@/components/home/WhyChooseUsSection';
+import EmotionalStorySection from '@/components/home/EmotionalStorySection';
+import RecipesSection from '@/components/home/RecipesSection';
+import LimitedTimeOfferSection from '@/components/home/LimitedTimeOfferSection';
+import TestimonialsSection from '@/components/home/TestimonialsSection';
 
 const Index: React.FC = () => {
   return (
@@ -14,11 +20,18 @@ const Index: React.FC = () => {
       
       <main>
         <HeroSection />
+        <LimitedTimeOfferSection />
+        <NewLaunchSection />
         <BestSellersSection />
+        <WhyChooseUsSection />
+        <EmotionalStorySection />
+        <RecipesSection />
+        <TestimonialsSection />
       </main>
 
       <Footer />
       
+      {/* Only WhatsApp Chat Widget - no duplicate */}
       <WhatsAppChatWidget />
     </div>
   );
